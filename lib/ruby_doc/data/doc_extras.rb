207180cb4de492b2f65b7f2a1c33863ccd50e69b
Alpha Daniel <daniel.nunez.nyc@gmail.com>
2018-01-22 17:17:52 +0000

    reificatiion: added control flow for pages with message stubs

--- lib/ruby_doc/data/doc_extras.rb
+++ lib/ruby_doc/data/doc_extras.rb
@@ -4,30 +4,65 @@ module DocExtras
     Doc.all[0..499].each_with_index do |doc, index|
       puts "#{index + 1}. " + doc.title
     end 
+    
+    puts "\nenter n for next page"
+    input = gets.strip.to_s.downcase
+    case input # SHUTTLE
+    when "n"
+      page2
+    end
   end
 #=====================================================================
   def page2
     Doc.all[500..999].each_with_index do |doc, index|
       puts "#{index + 1}. " + doc.title
     end 
+    
+    puts "\nenter n for next page"
+    input = gets.strip.to_s.downcase
+    case input # SHUTTLE
+    when "n"
+      page3
+    end
   end
 #=====================================================================  
   def page3
     Doc.all[1000..1499].each_with_index do |doc, index|
       puts "#{index + 1}. " + doc.title
-    end 
+    end
+    
+    puts "\nenter n for next page"
+    input = gets.strip.to_s.downcase
+    case input # SHUTTLE
+    when "n"
+      page4
+    end
   end
 #=====================================================================  
   def page4
     Doc.all[1500..1999].each_with_index do |doc, index|
       puts "#{index + 1}. " + doc.title
     end
+    
+    puts "\nenter n for next page"
+    input = gets.strip.to_s.downcase
+    case input # SHUTTLE
+    when "n"
+      last
+    end
   end
 #=====================================================================  
   def last
     Doc.all[2000..self.all.length].each_with_index do |doc, index|
       puts "#{index + 1}. " + doc.title
-    end 
+    end
+    
+    puts "\nenter m for main menu"
+    input = gets.strip.to_s.downcase
+    case input # SHUTTLE
+    when "m"
+      RubyDoc::CLI.start
+    end
   end 
 #=====================================================================
 end
