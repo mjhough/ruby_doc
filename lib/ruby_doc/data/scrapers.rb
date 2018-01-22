1b745af03445c11f430710552dc5a11d7e351bd3
Alpha Daniel <daniel.nunez.nyc@gmail.com>
2018-01-22 04:35:28 +0000

    scraper: targeted Doc :title

--- lib/ruby_doc/data/scrapers.rb
+++ lib/ruby_doc/data/scrapers.rb
@@ -1,9 +1,15 @@
 class Scraper
 #=================place_holder==================
   def self.place_holder
+    # load
     html = Nokogiri::HTML(open("https://apidock.com/ruby/browse"))
     container = html.search(".hover_list")
-    binding.pry
+    
+    # scrape
+    container.search("a").each do |doc|
+      doc_title = doc.text
+      binding.pry
+    end
   end
 #==============================================
 end
