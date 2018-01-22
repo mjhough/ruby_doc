80adc32c93bcc72df64d8ecbfb7f539f1c429f56
Alpha Daniel <daniel.nunez.nyc@gmail.com>
2018-01-22 03:22:19 +0000

    initial

---
+++ lib/environment.rb
@@ -0,0 +1,18 @@
+#===========Requires==========
+require 'require_all'
+require 'colorize'
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+require_all 'bin'
+require_all 'lib'
+#============Patch============
+def x; exit!; end #Quit Exit
+
+#=======================Load Docs==========================
+puts "Initial Loading!".cyan
+# LOAD_FILE_HERE
+
+#========================Testing=========================== 
+# Preload into environment to quick pry
+# binding.pry
---
+++ lib/ruby_doc/version.rb
@@ -0,0 +1,3 @@
+module RubyDoc
+  VERSION = "0.1.0"
+end
