163451f051b2aed1c3a1c72aa1afafd6f94ff657
Alpha Daniel <daniel.nunez.nyc@gmail.com>
2018-01-22 09:34:26 +0000

    stub: main menu

--- lib/ruby_doc/ui/UI.rb
+++ lib/ruby_doc/ui/UI.rb
@@ -4,6 +4,9 @@ module RubyDoc::CLI
 #==============================================
     def self.greeting
       puts "UI Greenlight".green
+      puts "\nEnter a word associated with the Ruby Language & I will "
+      puts "try to find a match in my database for you."
+      puts "\nYou can also type 'b' to browse instead. Happy Hunting!"
     end
 #==============================================    
   end
