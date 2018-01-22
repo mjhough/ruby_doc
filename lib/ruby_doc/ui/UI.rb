33e3bb2c9d96346e61cc02bb25ad180525433e4c
Alpha Daniel <daniel.nunez.nyc@gmail.com>
2018-01-22 10:24:47 +0000

    stub: browse menu

--- lib/ruby_doc/ui/UI.rb
+++ lib/ruby_doc/ui/UI.rb
@@ -3,14 +3,12 @@ module RubyDoc::CLI
   module UI
 #==============================================
     def self.greeting
-      puts "UI Greenlight".green
       puts "\nEnter a word associated with the Ruby Language & I will "
       puts "try to find a match in my database for you."
       puts "\nYou can also type 'b' to browse instead. Happy Hunting!"
     end
 #==============================================
     def self.mainControl
-      puts "mainControl Greenlight".green
       input = gets.strip.to_s.downcase
       puts "input #{input} downcase Greenlight".green
       
@@ -21,6 +19,9 @@ module RubyDoc::CLI
 #==============================================
     def self.browseControl
       puts "\nbrowseControl Greenlight".green
+      puts "\nTo Browse Docs (Enter '1')"
+      puts "To Browse Methods (Enter '2')"
+      puts "\nBack to Main Menu (Enter 'm')"
     end
 #==============================================    
   end
