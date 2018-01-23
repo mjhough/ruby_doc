ce68fff112b745820ad9b513ba84a96731cf05ff
Alpha Daniel <daniel.nunez.nyc@gmail.com>
2018-01-23 09:05:51 +0000

    uicandy: added UIExtras to maintain uiCandy + added separators (greeting)

--- lib/ruby_doc/ui/UI.rb
+++ lib/ruby_doc/ui/UI.rb
@@ -1,23 +1,25 @@
 module RubyDoc::CLI
-#==============================================
-  module UI
+#==================modules=====================
+  class UI
+    extend UIExtras#
+    #separators
 #==============================================
     def self.greeting
       puts "\n                 SIGNATURE GOES HERE".black
-      puts "=".cyan*28 + "=".white*28
+      separatorL#
      
       puts "Enter a ".cyan + "word ".yellow + "associated with the Ruby Language & I will ".cyan
       puts "try to find a match in my database for you.".cyan
-      puts "=".white*28 + "=".cyan*28
+      separatorR#
       
       puts "\You can also type".cyan + " 'b'".yellow + " to browse instead.".cyan + " Happy Hunting!".cyan
       
-      puts "\n If You're Searching... Single Word Inputs Only Please ".colorize(color: :white, background: :cyan)
+      print "\n If You're Searching... Single Word Inputs Only Please ".colorize(color: :white, background: :cyan)
     end
 #==============================================
     def self.mainControl
+      print " >> ".cyan
       input = gets.strip.to_s.downcase
-      puts "input #{input} downcase Greenlight".green
       
       case input
       when "b"
---
+++ lib/ruby_doc/ui/ui_extras.rb
@@ -0,0 +1,11 @@
+module UIExtras
+#=================Separators===================
+  def separatorL
+    puts "=".cyan*28 + "=".white*28
+  end
+    
+  def separatorR
+    puts "=".white*28 + "=".cyan*28
+  end
+#==============================================
+end
